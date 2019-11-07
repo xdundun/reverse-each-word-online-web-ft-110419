ee230cc4eb6ebfdcbf472206562857e0c88142ad
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 15:31:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(sentence)
   sentence_reverse = []
   counter = 0
   sentence.each do {
-    sentence_reverse = sentence
-    counter += 1
+    #sentence_reverse[counter] = sentence
+  #  counter += 1
+  puts sentence
   }
   end
   return sentence_reverse
